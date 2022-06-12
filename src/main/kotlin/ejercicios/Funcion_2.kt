16ef483fb9a11d57260c262ef5fa5ecd4a0b52b8
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-06-12 14:18:51 -0500

    Adhiriendo funciones

---
+++ src/main/kotlin/ejercicios/Funcion_2.kt
@@ -0,0 +1,17 @@
+package ejercicios
+
+import java.lang.Math.ceil
+
+fun main() {
+    print("Ingrese el No. de estudiantes gordos: ")
+    val gordos = readLine()!!.toInt()
+    print("Ingrese el No. de estudiantes flacos: ")
+    val flacos = readLine()!!.toInt()
+    print("Ingrese el No. de sillas: ")
+    val sillas = readLine()!!.toInt()
+    println("Se requieren ${calcularBuses(gordos, flacos, sillas)} buses")
+}
+
+fun calcularBuses(gordos: Int, flacos: Int, sillas: Int): Int {
+    return ceil(((gordos * 2.0) + flacos) / sillas).toInt()
+}
