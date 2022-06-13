b46f1af24da65000ce9f0c582779882e770796d0
Marco Vanegas <106780475+BambooThink@users.noreply.github.com>
2022-06-12 20:16:20 -0500

    Update Funcion_8.kt

--- src/main/kotlin/ejercicios/Funcion_8.kt
+++ src/main/kotlin/ejercicios/Funcion_8.kt
@@ -3,8 +3,9 @@ package ejercicios
 fun main() {
     print("Ingrese el total de estudiantes: ")
     val estudiantes = readLine()!!.toDouble()
+    //Pendiente
 }
 
 fun calcularPisos(estudiantes: Int) {
-
+    //Pendiente
 }
